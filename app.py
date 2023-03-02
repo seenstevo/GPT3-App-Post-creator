6ecac1d9eed1c6532978b8c0071679f19172dbf7
JaredR33 <118567250+JaredR33@users.noreply.github.com>
2023-03-02 17:22:49 +0100

    first commit

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
-from flask import Flask, request, render_template
-import openai
+from flask import Flask, render_template, request
 import os
+import pandas as pd
+import openai
 
-os.chdir(os.path.dirname(__file__))
-
-try:
-    openai.api_key = open("api_key.txt", "r").read()
-except:
-    print("No api_key.txt in the directory with your credentials")
-
-application = Flask(__name__)
+dir_script = os.path.dirname(os.path.abspath(__file__))
+#app para comunicar con chatgp3 y database en AWS
 
 
-@application.route('/')
-def my_form():
-    return render_template('index.html')
 
-@application.route('/', methods=['POST'])
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+print(os.getcwd())
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route("/")
+def index():
+    template = render_template(dir_script + '\\app\\templates\\index.html')
+    return template
+
+@app.route('/', methods=['POST'])
 def my_form_post():
-    variable = request.form['variable']
+    prompt = request.form['prompt']
+    
     response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt=variable,
-    max_tokens=300,
+    engine="text-davinci-003",
+    prompt=prompt,
+    max_tokens=1000,
     temperature=0.9
     )
     text_output = response.choices[0].text
@@ -30,6 +34,4 @@ def my_form_post():
     text_output = text_output.replace('\t', '    ')
     return text_output + '<p><a href="/">Back</a></p>\n'
 
-
-if __name__ == "__main__":
-    application.run()
+app.run()
