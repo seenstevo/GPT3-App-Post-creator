5ec5d79526e450bdaf6e619b68f2e46d6c2da2b4
seenstevo <seenstevo@protonmail.com>
2023-03-03 09:27:20 +0100

    localhost

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def my_form_post():
     return text_output + '<p><a href="/">Back</a></p>\n'
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = 'localhost', port = 5000)
